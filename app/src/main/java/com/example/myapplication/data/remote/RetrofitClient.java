f55e4cb89c1198a688189f3222ddde14e8393a17
macchinguyen123 <23130211@st.hcmuaf.edu.vn>
2026-06-24 14:18:56 +0700

    them qr

--- app/src/main/java/com/example/myapplication/data/remote/RetrofitClient.java
+++ app/src/main/java/com/example/myapplication/data/remote/RetrofitClient.java
@@ -5,9 +5,13 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
 
-    public static final String BASE_URL =
-            "http://10.0.2.2:8000/";
-//            "http://10.111.30.186:8000/";
+    // URL cho API calls từ giả lập Android (Emulator)
+    // 10.0.2.2 là địa chỉ đặc biệt để giả lập kết nối vào máy tính host
+    public static final String BASE_URL = "http://10.0.2.2:8000/";
+
+    // URL cho QR Code - dùng IP LAN thực của máy tính để điện thoại thật quét được
+    // Thay 10.51.180.224 bằng IP Wi-Fi thực của máy tính nếu thay đổi mạng. Có thể cập nhật động.
+    public static String QR_BASE_URL = "http://10.51.180.224:8000/";
 
     private static Retrofit retrofit;
 
